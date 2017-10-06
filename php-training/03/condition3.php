4453be5b441339a96373820d98139eef2d83955d
Nariaki-Sato <nariakif5@gmail.com>
2017-10-06 13:19:06 +0900

    Finish 03

---
+++ php-training/03/condition3.php
@@ -0,0 +1,9 @@
+<?php
+//$message = 'こんにちは！';
+//print $message ?? 'ノーコメント';
+//print isset($message) ? $message : 'ノーコメント';
+
+
+$message = '';
+print $message ?? 'ノーコメント';
+//print isset($message) ? $message : 'ノーコメント';
